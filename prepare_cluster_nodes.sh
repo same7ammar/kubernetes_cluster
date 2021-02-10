216f969e70ff0814647a5937a2edaa405e942580
same7ammar <mr.same7.ammar@gmail.com>
2021-02-10 13:49:01 +0300

    Update prepare_cluster_nodes.sh

--- prepare_cluster_nodes.sh
+++ prepare_cluster_nodes.sh
@@ -7,18 +7,14 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
+sudo apt-transport-https curl -y
+sudo apt -y install vim git curl wget kubelet kubeadm kubectl
 
 echo "Updates packages ....."
 sudo apt update -y
 sudo apt install -y docker-ce=5:19.03.10~3-0~ubuntu-focal kubelet=1.18.5-00 kubeadm=1.18.5-00 kubectl=1.18.5-00
 sudo systemctl start docker
 sudo systemctl enable docker
-
-
-echo "Install Kubernets master node --> ...."
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-sudo mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-RS=`kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml`
-echo $RS
+sudo systemctl status  docker
