3182ddb29a044bf73a73d83fa0b47885a32c592b
same7ammar <mr.same7.ammar@gmail.com>
2021-02-10 13:35:53 +0300

    Create worker_join_tokens.sh

---
+++ worker_join_tokens.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+echo "Listing All tokens ......"
+kubeadm tokens list
+echo "generate a token for new node ......"
+kubeadm token create --print-join-command
+echo "Please run the command on the node ...."
