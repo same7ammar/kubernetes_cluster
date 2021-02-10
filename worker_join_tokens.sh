15f29616953cf6943fafb359ffb5db749e14c081
same7ammar <mr.same7.ammar@gmail.com>
2021-02-10 16:10:12 +0300

    Update worker_join_tokens.sh

--- worker_join_tokens.sh
+++ worker_join_tokens.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Listing All tokens ......"
-kubeadm tokens list
+sudo kubeadm tokens list
 echo "generate a token for new node ......"
-kubeadm token create --print-join-command > /root/kubernetes_join.txt
+sudo kubeadm token create --print-join-command > /root/kubernetes_join.txt
 echo "Please run the command on the node ...."
